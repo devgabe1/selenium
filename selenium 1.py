d9ed6f1cebf95c42eb68b85f5847e863bf7003ae
Gabriel Schultz <gabrielschultz2807@gmail.com>
2024-12-19 15:12:07 -0300

    maximize_window()

--- selenium 1.py
+++ selenium 1.py
@@ -3,6 +3,10 @@ import time
 
 #abrir um navegador
 navegador = webdriver.Chrome()
+#acessar um site
 navegador.get("https://www.youtube.com")
+
+#maximizar a tela
+navegador.maximize_window()
 
 time.sleep(90)
