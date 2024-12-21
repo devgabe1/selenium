d698b14838bd1012875172f4e4eabfa731e4c45b
Gabriel Schultz <gabrielschultz2807@gmail.com>
2024-12-21 13:42:11 -0300

    Declarando elemento e usando a função click

--- selenium 1.py
+++ selenium 1.py
@@ -4,9 +4,14 @@ import time
 #abrir um navegador
 navegador = webdriver.Chrome()
 #acessar um site
-navegador.get("https://www.youtube.com")
+navegador.get("https://www.youtube.com/")
 
 #maximizar a tela
 navegador.maximize_window()
 
-time.sleep(90)
+#encontrar um elemento
+botao_procura = navegador.find_element("class name", "ytSearchboxComponentSearchForm''")
+#clicar no elemento
+botao_procura.click()
+
+time.sleep(10)
